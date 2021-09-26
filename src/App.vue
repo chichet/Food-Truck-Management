fa26b11ff2eb1ddbeade78ba7cd7819495d5ce24
Sun, Tian <tsun90@gatech.edu>
2021-09-26 17:56:27 -0400

    register page initialized

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <v-app>
     <v-main>
       <HelloWorld/>
+      <Register/>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld';
+import Register from './components/Register.vue';
 
 export default {
   name: 'App',
 
   components: {
     HelloWorld,
+    Register,
   },
 
   data: () => ({
